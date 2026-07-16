ccd96c2aa14fb4194de24438e8f2b55deac2db33
Vihaan Jim <vihaanjim@proton.me>
2026-07-16 08:24:54 -0400

    Add .dir-locals.el

---
+++ .dir-locals.el
@@ -0,0 +1,5 @@
+;;; Directory Local Variables            -*- no-byte-compile: t -*-
+;;; For more information see (info "(emacs) Directory Variables")
+
+((LaTeX-mode . ((fill-column . 65)
+		(TeX-master . "pathexpr.tex"))))
